96ffda24b79a893e415098636f60a4508440123c
Thiago-Offredi <thiagooffredils@gmail.com>
2022-09-26 15:22:37 -0300

    Praticando os exercicios propostos

---
+++ exercicios.js
@@ -0,0 +1,27 @@
+/*1-
+Faça um programa que SOME 2 números
+*/
+//const firstNumber = 3
+//const seccondNunber= 5
+//const result= firstNumber + seccondNunber
+//console.log(result)
+/*
+2-
+Faça um programa que multiplique 2 números e o resultado adicione 10
+*/
+//const firstNumber = 3
+//const seccondNunber = 6
+//const result = (firstNumber * seccondNunber) + 10
+//console.log(result)
+/*3-
+Faça um programa que encontre a raiz quadrada de um número, multiplique o resultado por 10 e divida por 33*/
+//const raiz = (Math.sqrt(16))
+//const result = (raiz * 10) / 33
+//console.log(result)
+/*
+4-Faça um programa que inicie com dois nomes, e o programa imprima na tela o seguinte dado:
+ Olá, meu nome é (nome 1) e meu partner do CodeClub é (nome2)
+*/
+const firstName= "Thiago"
+const seccondName= "Mateus"
+const print= `Olá meu nome é ${firstName}, e o nome do meu partner no Devclub é ${seccondName}`
